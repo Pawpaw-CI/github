d5d31442f592ef269c49b83d3b9f6852e8d49d0c
dxy <adu0213@163.com>
2017-05-23 14:38:43 +0800

    if判断逻辑实现

--- Introducing python-4.py
+++ Introducing python-4.py
@@ -1,2 +1,26 @@
 # -*-coding:utf-8-*-
 __author__ = "pawpawDu"
+# 60 sec/min,60min/hr,24hr/day
+#oython外壳：代码结构
+import time
+print time.timezone
+print time.localtime()
+alphabet = ""
+alphabet +="abdc"
+alphabet +="efgj"
+print(alphabet)
+#使用\连接，仍是一行
+alphabet = "abcd"+\
+    "efjk"+\
+    "QEWQEQ"+\
+    "ddf"
+print alphabet
+#4.3使用if elif else
+
+print("请输入：True or False")
+disaster = input()
+if disaster:
+    print "WW00!"
+else:
+    print "wjhjj!"
+print disaster
