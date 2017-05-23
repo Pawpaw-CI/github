2b68a1a963979ca206f1de8f3564a5213d83b3e1
dxy <adu0213@163.com>
2017-05-23 15:09:22 +0800

    if判断逻辑实现 双值判定嵌套实现，四种关系真真，真假，假真，假假

--- Introducing python-4.py
+++ Introducing python-4.py
@@ -16,11 +16,29 @@ alphabet = "abcd"+\
     "ddf"
 print alphabet
 #4.3使用if elif else
-
+#单值真假输入：0,1
 print("请输入：True or False")
-disaster = input()
+disaster = input()#输入参数：False假，输入数字或布尔值0,1
 if disaster:
-    print "WW00!"
+    print "正确的选择!"
 else:
-    print "wjhjj!"
+    print "错误的选择!"
 print disaster
+#多层嵌套
+#双值，4中关系，00,01,10,11
+furry = False
+small = True
+
+if furry:
+    if small:
+        print ("11，it's a cat.")
+    else:
+        print ("10，it' a bear")
+else:
+    if small:
+        print ("01，it's a skink")
+    else:
+        print ("00，it's a human ,or a hairless bear")
+
+
+
